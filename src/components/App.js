6fc8ab6fa4d0f55f9e9c7d273ba1d8804d7246fa
YouGuavas <patrickyambrick@gmail.com>
2018-05-15 13:14:21 -0400

    Finished tutorial

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
 	render() {
 		return(
 				<div>
-					<h1>Welcome to my starter app!</h1>
+					<h1>Welcome to My Starter App</h1>
 				</div>
 			)
 	}
