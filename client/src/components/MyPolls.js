3f99e111c246849c8550228f69b3c3ca215c54d5
YouGuavas <patrickyambrick@gmail.com>
2018-05-25 15:27:10 -0400

    Add MyPolls page, to show a user polls they've created.

---
+++ client/src/components/MyPolls.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {getPollsData} from '../utils/api';
+import Item from './Item';
+
+export default class MyPolls extends Component {
+	constructor() {
+		super();
+		this.state = {polls: []};
+	}
+	getPolls = () => {
+		getPollsData(this.props.user).then(polls => {
+			//return vote data, then update state
+			this.setState({polls});
+		});
+	}
+	componentDidMount() {
+		this.getPolls();
+	}
+	render() {
+		//console.log(this.props.user);
+		const {polls} = this.state;
+		return(
+		 <div>
+		 		<h1 className='title'>Your Polls</h1>
+				<p>These are polls you have created!</p>
+			<div className='polls'>
+				{polls.map((poll, index) => (<Item title={poll.poll.title} id={poll.id} url={poll._id} options={poll.poll.options} key={index}/>))}
+			</div>
+		</div>
+		)
+	}
+}
