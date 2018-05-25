76997ed2cfae1d6f5aa61d54e6c9a44a5a4ce588
YouGuavas <patrickyambrick@gmail.com>
2018-05-25 15:26:39 -0400

    Separate Item component into its own file.

---
+++ client/src/components/Item.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Item extends Component {
+	render() {
+		return(
+			<Link to={{pathname:`/polls/${this.props.url}`, state:{id: this.props.url, options: this.props.options, title: this.props.title}}}>
+				<div className='level'>
+					<div className='level-item'>
+						{this.props.title}
+					</div>
+				</div>
+			</Link>
+			)
+	}
+}
