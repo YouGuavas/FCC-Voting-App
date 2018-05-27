7cca2335c8be00adb83aca4410b5bcd72c04214d
YouGuavas <patrickyambrick@gmail.com>
2018-05-27 13:05:58 -0400

    Set up api for individual polls

--- client/src/components/Poll.js
+++ client/src/components/Poll.js
@@ -1,29 +1,43 @@
 import React, {Component} from 'react';
 import {PieChart, Pie, Cell, ResponsiveContainer} from 'recharts';
-import { vote } from '../utils/api';
+import { vote, getPollData } from '../utils/api';
 
 const RADIAN = Math.PI/180;
 const colors = ['#8884d8', 'red', 'green'];
 export default class Poll extends Component {
-	constructor(props) {
-		super(props);
-		const data = this.props.location.state;
-		const chartData = [];
-		Object.keys(data.options).map(key=>chartData.push({name: key, value: data.options[key], id: data.id}));
-		this.state = {
-			chartData,
-			data
-		}
+	constructor() {
+		super();
+		this.state = {}
+	}
+	getPoll =(pollId)=> {
+		getPollData(pollId).then(res => {
+			const poll = res.data.poll;
+			//return vote data, then update state
+			let chartData = [];
+			Object.keys(poll.options).map(item => {
+				chartData.push({name: item, value: poll.options[item]});
+			})
+			this.setState({
+				poll,
+				chartData
+			});
+		});
+	}
+	componentDidMount() {
+		const search = window.location.pathname.split('/')[2];
+		this.getPoll(search);
 	}
 	handleClick = () => {
 		const select = document.getElementById('voteSelect');
 		const name = select.value;
 		let chartData = this.state.chartData;
+		const search = window.location.pathname.split('/')[2];
 		chartData.map(item => {
+			console.log(item);
 			item.name === name ? (
 				item.value += 1,
 				//increase vote count in state
-				vote(`${item.id}/${name}`)
+				vote(`${search}/${name}`)
 				//increase vote count in database
 				)
 			 : null
@@ -33,27 +47,36 @@ export default class Poll extends Component {
 		});
 	}
 	render() {
-		const data = this.state.data;
+		const data = this.state.poll;
+		const url = window.location;
+		const tweet = `Check out my poll!`;
 		return(
 			<div>
+			{data ?
 				<h2 className='title'>
 					{data.title}
-				</h2>
-					<Chart chartData={this.state.chartData}/>
+				</h2> : null
+			}
+					{ this.state.chartData ? <Chart chartData={this.state.chartData}/> : null}
+				{data ? 
 				<div className='select'>
 					<select id='voteSelect'>
 						{Object.keys(data.options).map((key,index)=><option key={index}>{key}</option>)}
 					</select>
-				</div>
+				</div> : null }
+				{data ?
 				<button className='button' onClick={this.handleClick}>
 					Vote
-				</button>
-				<div>
+				</button> : null }
+				{this.state.chartData ? <div>
 					{
 					this.state.chartData.map(
 						(item, index) => (<p key={index}>{`${item.name}: ${item.value}`}</p>))}
-				</div>
-				{this.props.user.id === this.props.id ? <div>Tweetz!</div> : ''}
+				</div> : null }
+				{data ? 
+				<a href={`https://twitter.com/intent/tweet?url=${encodeURI(url)}&text=${encodeURI(tweet)}`} className='is-primary button'>
+					<span className='icon is-small'><i className='fa fa-twitter'></i></span> Share!
+				</a> : null}
 			</div>
 			)
 	}
@@ -69,7 +92,7 @@ class Chart extends Component {
 		//Ensure label text size never goes lower than min
 		const min = 15;
 		const pSize = (percent*40).toFixed(0);
-		console.log(percent)
+		//console.log(percent)
 		const fSize = (pSize > min) ? pSize : min;
 
 		return (
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:3333';
-export {getPollsData, vote, newPoll};
+export {getPollsData, vote, newPoll, getPollData};
 function getPollsData(user) {
 	//console.log(user);
 	const url = `${BASE_URL}/api/polls`;
 	return axios.post(url, user).then(res=>res.data);
+}
+function getPollData(poll) {
+	const url = `${BASE_URL}/api/poll/${poll}`;
+	return axios.get(url);
 }
 function vote(poll) {
 	const url = `${BASE_URL}/api/vote/${poll}`;
